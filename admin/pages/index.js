5281aacf787f77a9e8de8bb2454f2a9028435e52
SebastianLozanoValle <90271134+SebastianLozanoValle@users.noreply.github.com>
2023-09-17 01:22:11 -0500

    primer commit
    inicio del proyecto

---
+++ admin/pages/index.js
@@ -0,0 +1,23 @@
+
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session){
+    return (
+      <div className='bg-blue-900 w-screen h-screen flex items-center'>
+        <div className='text-center w-full'>
+          <button onClick={() => signIn('google')} className='bg-white p-2 px-4 rounded-lg'>Login With Google</button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      logged in {session.user.email}
+      <button onClick={() => signOut()}>Sign out</button>
+    </div>
+  )
+}
